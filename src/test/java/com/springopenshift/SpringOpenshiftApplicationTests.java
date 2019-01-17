2349a2566b0344fcb346c3d75d02b8cfcadaa7a4
hassetshi <hassetshi@gmail.com>
2019-01-17 10:12:43 -0500

    first commit

---
+++ src/test/java/com/springopenshift/SpringOpenshiftApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springopenshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringOpenshiftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
